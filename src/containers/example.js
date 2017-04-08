89969b870e1d0e6b8be45411b1250a5a424c7a00
DESKTOP-LJE78RD\Jorgen <jorgenvedom@gmai.com>
2017-04-08 18:10:55 +0300

    Animation display logic

--- src/containers/example.js
+++ src/containers/example.js
@@ -1,23 +1,63 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
 
 import { Button } from 'react-bootstrap';
 
+const CLICKS_BEFORE_ANIMATION = 5;
+
 class Example extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isButtonDisabled: false,
+            currentClicks: 0
+        };
+
+        this.setButtonEnabled = this.setButtonEnabled.bind(this);
+        this.increaseClicksMade = this.increaseClicksMade.bind(this);
+        this.setDisabledIfClicksFinished = this.setDisabledIfClicksFinished.bind(this);
+    }
+
+    increaseClicksMade() {
+        this.setState({currentClicks: this.state.currentClicks + 1}, this.setDisabledIfClicksFinished);
+    }
+
+    setDisabledIfClicksFinished() {
+        if (this.state.currentClicks === CLICKS_BEFORE_ANIMATION) {
+            this.setState({isButtonDisabled: true});
+        }
+    }
+
+    setButtonEnabled() {
+        this.setState({currentClicks: 0, isButtonDisabled: false});
+    }
+
+    getAnimationClassName(animationClass) {
+        return this.state.isButtonDisabled ? animationClass : '';
+    }
 
     render () {
         return (
             <div>
-                <p>Hello darkness my old fried</p>
-                <div id="animate"/>
-                <Button>Hello Click Me</Button>
-                <img src="../../static/dinosaur.png" id="animate" />
+                <Button onClick={this.increaseClicksMade}
+                        disabled={this.state.isButtonDisabled}>
+                    Hello Click Me
+                </Button>
+
+                <center>
+                    <img id="animate"
+                         className={this.getAnimationClassName('animationChest')}
+                         src="../../static/dinosaur.png"/>
+                </center>
+
+                <img id="chest"
+                     className={this.getAnimationClassName('animationChest')}
+                     src="../../static/Chest.png"
+                     onClick={this.setButtonEnabled}/>
             </div>
         );
     }
 }
 
-const mapStateToProps = (state) => ({
-});
-
-export default connect(mapStateToProps, dispatch => ({dispatch}))(Example);
+export default Example;

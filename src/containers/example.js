813e68b5702a5432853e613e1b634ddf1a15c081
Plekksaabas <jorgenvedom@gmail.com>
2017-04-08 17:07:44 +0300

    Button and div for upcoming animation.

--- src/containers/example.js
+++ src/containers/example.js
@@ -1,12 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import { Button } from 'react-bootstrap';
+
 class Example extends Component {
 
     render () {
         return (
             <div>
                 <p>Hello darkness my old fried</p>
+                <div id="animate"/>
+                <Button>Hello Click Me</Button>
             </div>
         );
     }
